b72b9d82958a14daad9896dfedf6a0fadc36e43f
Philippe Charrière <ph.charriere@gmail.com>
2022-07-01 06:04:30 +0000

    📝 save

--- 07-sat/as-serve.sh
+++ 07-sat/as-serve.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-SAT_HTTP_PORT=8083 sat ./as-hello-service/as-hello-service.wasm
+SAT_HTTP_PORT=8088 sat ./as-hello-service/as-hello-service.wasm
--- 07-sat/js-serve.sh
+++ 07-sat/js-serve.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-SAT_HTTP_PORT=8084 sat ./js-hello-service/js-hello-service.wasm
+SAT_HTTP_PORT=8089 sat ./js-hello-service/js-hello-service.wasm
